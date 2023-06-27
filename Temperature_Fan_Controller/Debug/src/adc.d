5b426946a1779602b83c638873ced042d66115c5
Marwan Fawzy Shahat Mahmoud <105066899+ArabianHindi@users.noreply.github.com>
2023-06-27 18:39:49 +0300

    Delete Temperature_Fan_Controller/Debug directory

--- Temperature_Fan_Controller/Debug/src/adc.d
+++
@@ -1,8 +0,0 @@
-src/adc.o src/adc.o: ../src/adc.c ../src/adc.h ../src/Macros.h \
-  ../src/std_types.h
-
-../src/adc.h:
-
-../src/Macros.h:
-
-../src/std_types.h:
--- Temperature_Fan_Controller/Debug/src/gpio.d
+++
@@ -1,8 +0,0 @@
-src/gpio.o src/gpio.o: ../src/gpio.c ../src/gpio.h ../src/std_types.h \
-  ../src/Macros.h
-
-../src/gpio.h:
-
-../src/std_types.h:
-
-../src/Macros.h:
